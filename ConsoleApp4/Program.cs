7f214d3f2e15b71e04583ecd88a7a913c151d594
conniekangniliu <269372910@qq.com>
2018-09-04 14:10:17 -0400

    Add files via upload

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    
+    class demo
+    {
+        delegate void testdelegate(string s); //method 1 定义委托
+        static void consolestr(string s )
+        {
+            Console.WriteLine(s);
+        }
+        static void Main(string[] args)
+        {
+            testdelegate idelegate = new testdelegate(consolestr); //method1 实例化委托
+            idelegate("1 instance delegate");//方法一 调用委托
+
+
+
+
+            testdelegate adelegate = delegate (string s) { Console.WriteLine(s); }; //method2 匿名委托
+
+
+
+
+
+            adelegate("2 anonymous delegate");
+
+
+            //method 3 lambda expression
+            testdelegate ldelegate = (s) => { Console.WriteLine(s); };
+            ldelegate("3 lambda delegate");
+            
+
+
+
+
+        }
+    }
+}
